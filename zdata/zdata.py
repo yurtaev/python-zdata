822f614661b0eec9d8d6d9d6e428881c469942a1
Egor Yurtaev <yurtaev.egor@gmail.com>
2011-03-01 21:23:22 +0600

    added: zdata.py

---
+++ zdata/zdata.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+
+import urllib
+import re
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+class zdata():
+    def __init__(self, username, password, api_key):
+        self.username = username
+        self.password = password
+        self.api_key = api_key
+        self.ticket = {
+            "ZohoWriter": None,
+            "ZohoSheet": None,
+            "ZohoShow": None
+        }
+        self.URL_TICKET = "https://accounts.zoho.com/login?servicename=%s&FROM_AGENT=true&LOGIN_ID=%s&PASSWORD=%s"
+
+    def get_ticket (self, servicename):
+        if not servicename in ["ZohoWriter", "ZohoSheet", "ZohoShow"]:
+            raise Exception('not servicename in ["ZohoWriter", "ZohoSheet", "ZohoShow"]')
+
+        if self.ticket[servicename]:
+            return self.ticket[servicename]
+
+        url = self.URL_TICKET % (servicename, self.username, self.password)
+
+        data = urllib.urlopen(url)
+        data = data.read()
+
+        result = re.findall("(?:RESULT=)(.*)", data)[0]
+        if result == "FALSE":
+            warning = re.findall("(?:CAUSE=)(.*)", data)[0]
+            raise Exception(warning)
+
+        self.ticket[servicename] = re.findall("(?:TICKET=)(.*)", data)[0]
+
+        return self.ticket[servicename]
+
+    def get_list(self, servicename, format="json", dict=False):
+        if not servicename in ["ZohoWriter", "ZohoSheet", "ZohoShow"]:
+            raise Exception('not servicename in ["ZohoWriter", "ZohoSheet", "ZohoShow"]')
+        if not format in ["xml", "json"]:
+            raise Exception('not format in ["xml", "json"]')
+
+        args = (format, self.api_key, self.get_ticket(servicename))
+        url = None
+
+        if servicename in "ZohoWriter":
+            url = "https://export.writer.zoho.com/api/private/%s/documents?apikey=%s&ticket=%s" % args
+        elif servicename in "ZohoSheet":
+            url = "https://sheet.zoho.com/api/private/%s/books?apikey=%s&ticket=%s" % args
+        elif servicename in "ZohoShow":
+            url = "http://show.zoho.com/api/private/%s/presentations?apikey=%s&ticket=%s" % args
+
+        data = urllib.urlopen(url)
+        data = data.read()
+
+        if format in "json":
+            if dict:
+                return json.loads(data)
+            else:
+                return data
+        elif format in "xml":
+            return data
